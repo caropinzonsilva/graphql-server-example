758c7d40c96a9c7283f89e39c2406c75cb10e572
Carolina Pinzon <carolina@axiomzen.co>
2019-02-15 09:30:07 -0800

    Revert "typeDefs + simple queries"
    This reverts commit 871301951af080df4c1fe18655fbfaf686fbc1d6.

--- index.js
+++ index.js
@@ -1,48 +1,34 @@
   const { ApolloServer, gql } = require('apollo-server');
-  const uuid = require('uuid/v4');
 
   // This is a (sample) collection of books we'll be able to query
   // the GraphQL server for.  A more complete example might fetch
   // from an existing data source like a REST API or database.
-  const database = {
-    books: {
-      'b9d110ec-b3c1-4ecc-9fc7-fd1c22b4521a': {
-        id: 'b9d110ec-b3c1-4ecc-9fc7-fd1c22b4521a',
-        title: 'Cien años De soledad',
-        authorId: 'd9e63480-4729-4b76-be00-bdc4b5c913a1',
-      },
-      '73488bd3-44fa-412b-94a1-2be9d0441c45': {
-        id: '73488bd3-44fa-412b-94a1-2be9d0441c45',
-        title: 'El amor en los tiempos del cólera',
-        authorId: 'd9e63480-4729-4b76-be00-bdc4b5c913a1',
-      }
+  const books = [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
     },
-    authors: {
-      'd9e63480-4729-4b76-be00-bdc4b5c913a1': {
-        id: 'd9e63480-4729-4b76-be00-bdc4b5c913a1',
-        name: 'Gabriel García Márquez',
-        books: ['b9d110ec-b3c1-4ecc-9fc7-fd1c22b4521a', '73488bd3-44fa-412b-94a1-2be9d0441c45']
-      }
+    {
+      title: 'Jurassic Park',
+      author: 'Michael Crichton',
     },
-  };
+  ];
+
   // Type definitions define the "shape" of your data and specify
   // which ways the data can be fetched from the GraphQL server.
   const typeDefs = gql`
+    # Comments in GraphQL are defined with the hash (#) symbol.
+
+    # This "Book" type can be used in other type declarations.
     type Book {
-      id: String!
-      title: String!
-      authorId: String!
+      title: String
+      author: String
     }
-    
-    type Author {
-      id: String!
-      name: String!
-      books: [Book]
-    }
-    
+
+    # The "Query" type is the root of all GraphQL queries.
+    # (A "Mutation" type will be covered later on.)
     type Query {
       books: [Book]
-      authors: [Author]
     }
   `;
 
@@ -50,18 +36,19 @@
   // schema.  We'll retrieve books from the "books" array above.
   const resolvers = {
     Query: {
-      books: () => Object.values(database.books),
-      authors: () => Object.values(database.authors),
+      books: () => books,
     },
   };
 
   // In the most basic sense, the ApolloServer can be started
   // by passing type definitions (typeDefs) and the resolvers
   // responsible for fetching the data for those types.
-
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    engine: process.env.ENGINE_API_KEY && {
+      apiKey: process.env.ENGINE_API_KEY,
+    },
   });
 
   // This `listen` method launches a web-server.  Existing apps
