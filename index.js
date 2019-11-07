aa6ec750b4c3b6ead69ed2712c1a52759726a949
Carolina Pinzón <carolina@axiomzen.co>
2019-11-07 15:00:44 -0500

    update mock

--- index.js
+++ index.js
@@ -21,30 +21,41 @@ const database = {
     'author_1': {
       id: 'author_1',
       name: 'Gabriel García Márquez',
-      books: ['b9d110ec-b3c1-4ecc-9fc7-fd1c22b4521a', '73488bd3-44fa-412b-94a1-2be9d0441c45']
+      books: ['book_1', 'book_2']
     }
   },
 };
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-    type Book {
-      id: String!
-      title: String!
-      authorId: String!
-    }
-    
-    type Author {
-      id: String!
-      name: String!
-      books: [Book]
-    }
-    
-    type Query {
-      books: [Book]
-      authors: [Author]
-    }
-  `;
+  type Book {
+    id: String!
+    title: String!
+    authorId: String!
+  }
+  
+  type Author {
+    id: String!
+    name: String!
+    books: [Book]
+  }
+  
+  union SearchResult = Book | Author
+  type Query {
+    books: [Book]
+    authors: [Author]
+    bookById(id: String!): Book
+    authorById(id: String!): Author
+    search(text: String): [SearchResult]
+  }
+  
+  input BookInput {
+    title: String!
+  }
+  type Mutation {
+    addAuthor(name: String!, books: [BookInput]): Author!
+  }
+`;
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
@@ -52,6 +63,43 @@ const resolvers = {
   Query: {
     books: () => Object.values(database.books),
     authors: () => Object.values(database.authors),
+    bookById: (_, { id }) => database.books[id],
+    authorById: (_, { id }) => database.authors[id],
+    search: (_, { text }) => ([
+      ...Object.values(database.books).filter((book) => book.title.toLowerCase().includes(text.toLowerCase())),
+      ...Object.values(database.authors).filter((author) => author.name.toLowerCase().includes(text.toLowerCase()))
+    ]),
+  },
+  Author: {
+    books: ({ id }) => Object.values(database.books).filter(book => book.authorId === id),
+  },
+  SearchResult: {
+    __resolveType: searchResult => (searchResult.name ? 'Author' : 'Book'),
+  },
+  Mutation: {
+    addAuthor: (_, { name, books = [] }) => {
+      const authorId = uuid();
+      const booksWithId = books.reduce(
+          (acc, book) => {
+            const bookId = uuid();
+            acc[bookId] = {
+              id: bookId,
+              title: book.title,
+              authorId,
+            };
+            return acc;
+          },
+          {}
+      );
+      const author = {
+        id: authorId,
+        name,
+        books: Object.keys(booksWithId),
+      };
+      database.authors[author.id] = author;
+      Object.assign(database.books, booksWithId);
+      return author;
+    },
   },
 };
 
